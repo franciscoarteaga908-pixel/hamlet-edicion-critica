a498e04be44dcf67dda18140a6c6b45605624ae1
franciscoarteaga908-pixel <francisco.arteaga908@gmail.com>
2025-11-24 11:19:21 -0500

    Delete CETEIcean directory

--- CETEIcean/js/apparatus.js
+++
@@ -1,9 +0,0 @@
-// Toggle individual <app> bubbles
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("tei-app").forEach(app => {
-    app.addEventListener("click", function(event) {
-      event.stopPropagation();
-      this.classList.toggle("open");
-    });
-  });
-});
